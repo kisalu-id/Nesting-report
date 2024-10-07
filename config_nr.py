1cd591d621adea1fcf35f2504274a35da3b4f8c1
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-10-07 22:30:27 +0200

    Update config_nr.py

--- config_nr.py
+++ config_nr.py
@@ -14,19 +14,14 @@ def run_config():
     cfg_file.close()
     cfg = ConfigHelperINI(ini_path)
 
-    cfg.add_parameter('Einstellung', 'Einstellung\\rotate', 'Möchten Sie die Platten hochkant drehen?', ConfigParamType.BOOLEAN, "1")
-    cfg.add_parameter('Einstellung', 'Einstellung\\delete_folder', 'Wen Ordner bereits existiert, soll er gelöscht werden', ConfigParamType.BOOLEAN, "2")
-
-    cfg.add_parameter('Pfad', 'Pfad\\report_pfad', 'Report Pfad wählen', ConfigParamType.DIRECTORY, ewd.explode_file_path('%TEMPPATH%'))
-
-    cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\nice_design', '*Funktion in Entwicklung* Deaktivieren für schwarz-weiß drucken, aktivieren für ausgefallenes Design', ConfigParamType.BOOLEAN, False)
+    cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\nice_design', Deaktivieren für schwarz-weiß drucken, aktivieren für ausgefallenes Design', ConfigParamType.BOOLEAN, False)
     cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\reports_pdfs_together', 'Effizienzbericht zusammen mit Gesamteffizienzbericht in einem PDF', ConfigParamType.BOOLEAN, False)
     cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\only_measures_BW', '*Funktion in Entwicklung* Nur Maßnahmenbericht', ConfigParamType.BOOLEAN, False)
+    
+    cfg.add_parameter('Pfad', 'Pfad\\report_pfad', 'Report Pfad wählen', ConfigParamType.DIRECTORY, ewd.explode_file_path('%TEMPPATH%'))
 
     cfg.run()
 
 
-
-
-if __name__ == '__main__':
+if name == 'main':
     run_config()
