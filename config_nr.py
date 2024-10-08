52e8dc61b00ca7203761800b9fc2771f405aea48
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-10-08 21:19:21 +0200

    Update config_nr.py

--- config_nr.py
+++ config_nr.py
@@ -14,11 +14,17 @@ def run_config():
     cfg_file.close()
     cfg = ConfigHelperINI(ini_path)
 
-    cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\nice_design', Deaktivieren für schwarz-weiß drucken, aktivieren für ausgefallenes Design', ConfigParamType.BOOLEAN, False)
+    cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\nice_design', 'Deaktivieren für schwarz-weiß drucken, aktivieren für ausgefallenes Design', ConfigParamType.BOOLEAN, False)
     cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\reports_pdfs_together', 'Effizienzbericht zusammen mit Gesamteffizienzbericht in einem PDF', ConfigParamType.BOOLEAN, False)
     cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\only_measures_BW', '*Funktion in Entwicklung* Nur Maßnahmenbericht', ConfigParamType.BOOLEAN, False)
-    
+    cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\divide_material', 'Teile den Bericht nach Material und Dicke auf', ConfigParamType.BOOLEAN, False)
+    cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\rotate', 'Möchten Sie die Platten hochkant drehen?', ConfigParamType.BOOLEAN, False)
+
+
+    cfg.add_parameter('Druckeinstellungen', 'Druckeinstellungen\\mini_report', 'Mini-report', ConfigParamType.BOOLEAN, False)
+
     cfg.add_parameter('Pfad', 'Pfad\\report_pfad', 'Report Pfad wählen', ConfigParamType.DIRECTORY, ewd.explode_file_path('%TEMPPATH%'))
+
 
     cfg.run()
 
